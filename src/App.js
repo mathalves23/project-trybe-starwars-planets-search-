48355356ed63917ff5ebeb561d92de10e88b3bba
Matheus de Araújo Alves <matheus.aalves@MacBook-Air-de-Matheus.local>
2022-02-25 22:57:43 -0300

    Ajeitando o requisito 2 e introduzindo o CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Background from './images/background.jpg';
 import Provider from './context/Provider';
 import Table from './components/Table';
 import Search from './components/Search';
 
 function App() {
   return (
-    <div>
+    <div style={ { backgroundImage: `url(${Background})` } }>
+      {/* Source: https://www.freecodecamp.org/news/react-background-image-tutorial-how-to-set-backgroundimage-with-inline-css-style/ */}
       <h1> Star Wars Planets Search </h1>
       <Provider>
         <Search />
